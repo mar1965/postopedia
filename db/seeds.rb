97b43d336e4065505f11002ac6d752fbf7ba977e
Martin Soler <martinrsoler@yahoo.com>
2017-02-13 10:49:53 -1000

    Postopedia User Roles, seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,26 @@
-require 'random_data'
+require 'faker'
 
-50.times do
+# Create Users
+5.times do
+  User.create!(
+      #name:     RandomData.random_name,
+      email:    Faker::Internet.email,
+      password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+# Create Wikis
+25.times do
   Wiki.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    title: Faker::Hipster.sentence(3, true, 4),
+    body: Faker::Hipster.paragraph(2, true, 4),
+    private: Faker::Number.between(0, 2),
+    user: users.sample
   )
 end
 wikis = Wiki.all
 
 puts "Finished seeding"
-puts "#{{Wiki.count}} wikis created"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
